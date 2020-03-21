c6ec4d7799916cac23d6e50a970d91067269f581
jonelantha <57597570+jonelantha@users.noreply.github.com>
2020-03-21 17:09:13 +0000

    chore: Move release process to github actions and semantic-release

---
+++ release.config.js
@@ -0,0 +1,55 @@
+const { execSync } = require('child_process');
+
+module.exports = isDryRun() ? getDryRunConfig() : getNormalConfig();
+
+function getDryRunConfig() {
+  return {
+    repositoryUrl: getLocalRepoUrl(),
+    branches: getCurrentBranch(),
+    plugins: [
+      '@semantic-release/commit-analyzer',
+      '@semantic-release/release-notes-generator',
+    ],
+  };
+}
+
+function getNormalConfig() {
+  return {
+    plugins: [
+      '@semantic-release/commit-analyzer',
+      '@semantic-release/release-notes-generator',
+      [
+        '@semantic-release/changelog',
+        {
+          changelogTitle: '# React Responsive Pagination Changelog',
+        },
+      ],
+      '@semantic-release/npm',
+      '@semantic-release/github',
+      [
+        '@semantic-release/git',
+        {
+          assets: ['CHANGELOG.md', 'package.json'],
+        },
+      ],
+    ],
+  };
+}
+
+function isDryRun() {
+  return process.argv.includes('--dry-run');
+}
+
+function getLocalRepoUrl() {
+  const topLevelDir = execSync('git rev-parse --show-toplevel')
+    .toString()
+    .trim();
+
+  return `file://${topLevelDir}/.git`;
+}
+
+function getCurrentBranch() {
+  return execSync('git rev-parse --abbrev-ref HEAD')
+    .toString()
+    .trim();
+}
