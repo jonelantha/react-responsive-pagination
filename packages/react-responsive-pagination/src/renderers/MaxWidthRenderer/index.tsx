c3de1edff3f7f30d8f582c8b267f601720234cdd
jonelantha <57597570+jonelantha@users.noreply.github.com>
2021-04-20 18:47:58 +0100

    refactor: Move renderer logic into hooks

--- packages/react-responsive-pagination/src/renderers/MaxWidthRenderer/index.tsx
+++ packages/react-responsive-pagination/src/renderers/MaxWidthRenderer/index.tsx
@@ -1,76 +1,26 @@
-import React, { Ref, forwardRef, useRef, useLayoutEffect } from 'react';
-import { ViewComponent, ViewItem } from '../../view';
-import { lastWhere, iteratorNext } from '../../helpers/iterator';
-import { useWidthCalculator } from './useWidthCalculator';
-import { useFoutDetector } from './useFoutDetector';
+import React, { useLayoutEffect } from 'react';
+import useWidestCompositionForWidth from '../useWidestCompositionForWidth';
+import { ViewItem, ViewComponent } from '../../view';
 
-const MaxWidthRenderer = forwardRef<HTMLElement, Props>(
-  ({ maxWidth, narrowToWideCompositionsProvider, View }, forwardedViewRef) => {
-    const widthCalculator = useWidthCalculator();
+export default function MaxWidthRenderer({
+  maxWidth,
+  narrowToWideCompositionsProvider,
+  View,
+}: Props) {
+  const { items, ref, clearCache } = useWidestCompositionForWidth(
+    narrowToWideCompositionsProvider,
+    maxWidth,
+  );
 
-    const containerElementRef = useRef<HTMLElement | null>(null);
+  useLayoutEffect(() => {
+    return () => clearCache();
+  }, [View, clearCache]);
 
-    const clearCache = widthCalculator.clearCache;
-
-    useFoutDetector(
-      () => getItemsDomElements(containerElementRef.current),
-      clearCache,
-    );
-
-    useLayoutEffect(() => {
-      return () => clearCache();
-    }, [View, clearCache]);
-
-    let items: ViewItem[];
-    let ref: Ref<HTMLElement>;
-
-    if ('renderNeeded' in widthCalculator) {
-      items = widthCalculator.renderNeeded.items;
-      ref = function (containerElement) {
-        widthCalculator.renderNeeded.ref(containerElement);
-        typeof forwardedViewRef === 'function' && forwardedViewRef(containerElement);
-        containerElementRef.current = containerElement;
-      };
-    } else {
-      items = getLargestFittingCompositionWithFallback(
-        narrowToWideCompositionsProvider,
-        widthCalculator.calculator,
-        maxWidth,
-      );
-      ref = function (containerElement) {
-        typeof forwardedViewRef === 'function' && forwardedViewRef(containerElement);
-        containerElementRef.current = containerElement;
-      };
-    }
-
-    return <View items={items} ref={ref} />;
-  },
-);
-
-export default MaxWidthRenderer;
+  return <View items={items} ref={ref} />;
+}
 
 type Props = {
   maxWidth: number;
   narrowToWideCompositionsProvider: () => IterableIterator<ViewItem[]>;
   View: ViewComponent;
 };
-
-function getLargestFittingCompositionWithFallback(
-  getNarrowToWideCompositions: () => IterableIterator<ViewItem[]>,
-  getCompositionWidth: (items: ViewItem[]) => number,
-  maxWidth: number,
-) {
-  const narrowToWideCompositions = getNarrowToWideCompositions();
-
-  const firstComposition = iteratorNext(narrowToWideCompositions) ?? [];
-
-  const doesCompositionFit = (composition: ViewItem[]) => {
-    return getCompositionWidth(composition) < maxWidth;
-  };
-
-  return lastWhere(narrowToWideCompositions, doesCompositionFit) ?? firstComposition;
-}
-
-function getItemsDomElements(viewDomElement: HTMLElement | null) {
-  return viewDomElement && (Array.from(viewDomElement.children) as HTMLElement[]);
-}
---
+++ packages/react-responsive-pagination/src/renderers/useWidestCompositionForWidth/index.tsx
@@ -0,0 +1,66 @@
+import { ForwardedRef, useRef } from 'react';
+import { ViewItem } from '../../view';
+import { lastWhere, iteratorNext } from '../../helpers/iterator';
+import { useWidthCalculator } from './useWidthCalculator';
+import { useFoutDetector } from './useFoutDetector';
+
+export default function useWidestCompositionForWidth(
+  narrowToWideCompositionsProvider: () => IterableIterator<ViewItem[]>,
+  maxWidth: number,
+): {
+  items: ViewItem[];
+  ref: ForwardedRef<HTMLElement | null>;
+  clearCache: () => void;
+} {
+  const widthCalculator = useWidthCalculator();
+
+  const containerElementRef = useRef<HTMLElement | null>(null);
+
+  const clearCache = widthCalculator.clearCache;
+
+  useFoutDetector(
+    () => getItemsDomElements(containerElementRef.current),
+    clearCache,
+  );
+
+  if ('renderNeeded' in widthCalculator) {
+    return {
+      items: widthCalculator.renderNeeded.items,
+      ref(containerElement) {
+        widthCalculator.renderNeeded.ref(containerElement);
+        containerElementRef.current = containerElement;
+      },
+      clearCache,
+    };
+  } else {
+    return {
+      items: getLargestFittingCompositionWithFallback(
+        narrowToWideCompositionsProvider,
+        widthCalculator.calculator,
+        maxWidth,
+      ),
+      ref: containerElementRef,
+      clearCache,
+    };
+  }
+}
+
+function getLargestFittingCompositionWithFallback(
+  getNarrowToWideCompositions: () => IterableIterator<ViewItem[]>,
+  getCompositionWidth: (items: ViewItem[]) => number,
+  maxWidth: number,
+) {
+  const narrowToWideCompositions = getNarrowToWideCompositions();
+
+  const firstComposition = iteratorNext(narrowToWideCompositions) ?? [];
+
+  const doesCompositionFit = (composition: ViewItem[]) => {
+    return getCompositionWidth(composition) < maxWidth;
+  };
+
+  return lastWhere(narrowToWideCompositions, doesCompositionFit) ?? firstComposition;
+}
+
+function getItemsDomElements(viewDomElement: HTMLElement | null) {
+  return viewDomElement && (Array.from(viewDomElement.children) as HTMLElement[]);
+}
