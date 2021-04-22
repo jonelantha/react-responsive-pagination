ee5c203d2e9038769ef9809139e44a537cff902d
jonelantha <57597570+jonelantha@users.noreply.github.com>
2021-04-22 08:54:58 +0100

    Refactor: extract out usePaginationItems

--- packages/react-responsive-pagination/src/index.tsx
+++ packages/react-responsive-pagination/src/index.tsx
@@ -1,37 +1,22 @@
 import React, { memo } from 'react';
 import PropTypes from 'prop-types';
-import { narrowToWideCompositions } from './compositions';
 import { BootstrapSkin, SkinComponent } from './skins';
 import { useView, PageChangeHandler } from './view';
-import { sanatizeInteger } from './helpers/util';
-import { useWidestComposition } from './hooks/useWidestComposition';
+import { usePaginationItems } from './hooks/usePaginationItems';
 
 export { SkinComponent };
 
 export default memo(Pagination);
 
 function Pagination({
-  current: propsCurrent,
-  total: propsTotal,
+  current,
+  total,
   onPageChange: handlePageChange,
   maxWidth,
 }: Props) {
-  const total = sanatizeInteger(propsTotal);
+  const { items, ref } = usePaginationItems(current, total, maxWidth);
 
-  const current =
-    total < 1 ? null : Math.max(1, Math.min(sanatizeInteger(propsCurrent), total));
-
-  const Skin = BootstrapSkin;
-
-  const View = useView(Skin, handlePageChange);
-
-  const narrowToWideCompositionsProvider = () =>
-    narrowToWideCompositions(current, total);
-
-  const { items, ref } = useWidestComposition(
-    narrowToWideCompositionsProvider,
-    maxWidth,
-  );
+  const View = useView(BootstrapSkin, handlePageChange);
 
   return items.length > 0 ? <View items={items} ref={ref} /> : null;
 }
