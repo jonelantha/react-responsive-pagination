5506bea6c8eb9d9a4ae3702a2e4d87ec9277cba8
jonelantha <57597570+jonelantha@users.noreply.github.com>
2021-07-19 18:53:11 +0100

    docs: smoothly change padding for live demo

--- packages/docs/src/templates/standalone.tsx
+++ packages/docs/src/templates/standalone.tsx
@@ -11,9 +11,25 @@ import GlobalStyles, {
 } from '../components/GlobalStyles';
 import MarkdownContent from '../components/MarkdownContent';
 
+const baseHPadding = '0.5rem';
+
+const extraHPaddingStartPx = 500;
+const extraHPaddingEndPx = 800;
+const extraHPaddingSizePx = 16;
+
+// padding = width * gradient + constant
+// padding = width * padding / (end - start) - padding * end * / (end - start)
+// (* 100 to turn the gradient into vw units)
+const extraHPaddingGradientVw =
+  (extraHPaddingSizePx / (extraHPaddingEndPx - extraHPaddingStartPx)) * 100;
+const extraHPaddingConstantPx =
+  -(extraHPaddingSizePx * extraHPaddingStartPx) /
+  (extraHPaddingEndPx - extraHPaddingStartPx);
+
 const StandalonePageStyles = createGlobalStyle`
   body {
-    padding: ${spacingVertical} ${math(`${spacingHorizontal} + 0.5rem`)};
+    --extra-h-padding: clamp(0px, ${extraHPaddingGradientVw}vw + ${extraHPaddingConstantPx}px, ${extraHPaddingSizePx}px);
+    padding: ${spacingVertical} calc(${baseHPadding} + var(--extra-h-padding));
   }
 `;
 
