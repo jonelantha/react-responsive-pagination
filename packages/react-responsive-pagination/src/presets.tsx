8aceb81c237cc157cdc50cdadf6cb94bbaa8f214
jonelantha <57597570+jonelantha@users.noreply.github.com>
2023-03-18 09:28:36 +0000

    feat: Move presets/labelBehavior to sub-packages

---
+++ packages/react-responsive-pagination/src/presets.tsx
@@ -0,0 +1,10 @@
+import { srOnlySpanLabel } from './labelBehaviour.js';
+
+export const v1_bootstrap4PaginationPreset = {
+  ariaCurrentAttr: false,
+  labelBehaviour: srOnlySpanLabel(),
+};
+
+export const bootstrap4PaginationPreset = {};
+
+export const bootstrap5PaginationPreset = {};
