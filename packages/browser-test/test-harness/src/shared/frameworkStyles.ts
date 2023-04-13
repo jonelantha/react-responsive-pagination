6c70e8d9d579dfffb6c05ab428324e337d935bfb
jonelantha <57597570+jonelantha@users.noreply.github.com>
2023-04-13 13:26:06 +0100

    feat: Themes

--- packages/browser-test/test-harness/src/shared/frameworkStyles.ts
+++ packages/browser-test/test-harness/src/shared/frameworkStyles.ts
@@ -4,6 +4,9 @@ const frameworkStyles = {
   bootstrap400: lazy(() => import('./Bootstrap4_0_0Styles')),
   bootstrap4: lazy(() => import('./Bootstrap4Styles')),
   bootstrap5: lazy(() => import('./Bootstrap5Styles')),
+  bootstrapLightTheme: lazy(() => import('./BootstrapLightTheme')),
+  minimalLightTheme: lazy(() => import('./MinimalLightTheme')),
+  classicLightTheme: lazy(() => import('./ClassicLightTheme')),
 };
 
 export const frameworkIds = Object.keys(frameworkStyles);
