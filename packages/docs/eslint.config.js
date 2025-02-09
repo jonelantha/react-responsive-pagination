e52e6046ae5a5179192352577a45aee5596d61ea
jonelantha <57597570+jonelantha@users.noreply.github.com>
2025-02-09 19:02:44 +0000

    docs: linting improvement + fixes

--- packages/docs/eslint.config.js
+++ packages/docs/eslint.config.js
@@ -1,8 +1,34 @@
-import eslintPluginAstro from 'eslint-plugin-astro';
+// @ts-check
 
-export default [
-  ...eslintPluginAstro.configs.recommended,
+import astroEslintParser from 'astro-eslint-parser';
+import eslintPluginAstro from 'eslint-plugin-astro';
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  eslintPluginAstro.configs.recommended,
+  eslintPluginAstro.configs['flat/jsx-a11y-recommended'],
+  tseslint.configs.recommended,
   {
-    rules: {},
+    files: ['**/*.astro'],
+    languageOptions: {
+      parser: astroEslintParser,
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro'],
+      },
+    },
   },
-];
+  {
+    ignores: ['dist', '.astro'],
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-unused-expressions': [
+        'error',
+        { allowShortCircuit: true },
+      ],
+    },
+  },
+);
