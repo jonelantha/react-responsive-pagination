6b289e4b8099efa735004ca8aacc321155065af5
jonelantha <57597570+jonelantha@users.noreply.github.com>
2025-05-23 20:33:53 +0100

    docs: star count and responsive improvements

--- packages/docs/src/utils.ts
+++ packages/docs/src/utils.ts
@@ -5,3 +5,20 @@ export function getPageUrl(doc: CollectionEntry<'docs'>, hash?: string) {
 
   return hash ? `${path}#${hash}` : path;
 }
+
+let starCount: string | undefined;
+export async function getStarCount() {
+  if (starCount === undefined) {
+    const response = await fetch(
+      'http://api.github.com/repos/jonelantha/react-responsive-pagination',
+    );
+    const data = await response.json();
+
+    starCount = data.stargazers_count;
+
+    if (typeof starCount !== 'number' || Number.isNaN(starCount)) {
+      throw `Unexpected starcount ${starCount}`;
+    }
+  }
+  return starCount;
+}
