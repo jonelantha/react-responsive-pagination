032419eab4e68e1246aa7f1fdcb56f88b16f89fa
jonelantha <57597570+jonelantha@users.noreply.github.com>
2025-09-26 20:13:20 +0100

    build: eslint config improvements

--- packages/test-harness/eslint.config.js
+++ packages/test-harness/eslint.config.js
@@ -1,11 +1,17 @@
 // @ts-check
 
 import eslint from '@eslint/js';
+import { defineConfig } from 'eslint/config';
 import tseslint from 'typescript-eslint';
 import reactPlugin from 'eslint-plugin-react';
 import pluginReactHooks from 'eslint-plugin-react-hooks';
 
-export default tseslint.config({
+export default defineConfig({
+  languageOptions: {
+    parserOptions: {
+      tsconfigRootDir: import.meta.dirname,
+    },
+  },
   settings: {
     react: {
       version: 'detect',
