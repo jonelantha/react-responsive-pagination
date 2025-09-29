5a9a9793f1cc855b8a298b7f7f2259a9b18f5d65
jonelantha <57597570+jonelantha@users.noreply.github.com>
2025-09-29 18:57:21 +0100

    test: transition renderNav test to playwright test runner

---
+++ packages/browser-test/test/renderNav.spec.ts
@@ -0,0 +1,28 @@
+import { test as base, expect } from '@playwright/test';
+import { TestHarnessPage } from './test-harness-page';
+
+export const test = base.extend<{
+  testHarness: TestHarnessPage;
+}>({
+  testHarness: async ({ page }, use) => {
+    const testHarness = new TestHarnessPage(page, { throwOnError: true });
+
+    await testHarness.goto();
+
+    await page.setViewportSize({ width: 700, height: 700 });
+
+    await use(testHarness);
+  },
+});
+
+test.describe('renderNav', () => {
+  for (const renderNav of [undefined, false, true]) {
+    test(`Setting renderNav to ${renderNav}`, async ({ testHarness }) => {
+      await testHarness.setField('renderNav', renderNav);
+
+      const paginationHtml = await testHarness.getPaginationHtml();
+
+      expect(paginationHtml).toMatchSnapshot();
+    });
+  }
+});
