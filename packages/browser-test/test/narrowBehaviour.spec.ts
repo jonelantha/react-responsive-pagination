a6242e989c8f2f55e8f0de5df59679275d6184b1
jonelantha <57597570+jonelantha@users.noreply.github.com>
2025-09-29 18:52:45 +0100

    test: transition linkHref, maxWidth, narrowBehaviour, naigation tests to playwright test runner

---
+++ packages/browser-test/test/narrowBehaviour.spec.ts
@@ -0,0 +1,54 @@
+import { test as base, expect } from '@playwright/test';
+import { TestHarnessPage } from './test-harness-page';
+
+const testWidths = [150, 200, 250];
+
+const narrowBehaviours = [
+  undefined,
+  'dropNav',
+  'dropEllipsis',
+  'dropFirstAndLast',
+  'dropNavThenEllipsis',
+  'dropEllipsisThenNav',
+  ['dropEllipsis', 'dropNav'],
+  ['dropNav', 'dropEllipsis'],
+  ['dropFirstAndLast', 'dropEllipsis'],
+  ['dropEllipsis', 'dropFirstAndLast'],
+];
+
+base.describe('Auto sizing', () => {
+  for (const narrowBehaviour of narrowBehaviours) {
+    const testWithBehaviour = base.extend<{
+      testHarness: TestHarnessPage;
+    }>({
+      testHarness: async ({ page }, use) => {
+        const testHarness = new TestHarnessPage(page, { throwOnError: true });
+
+        await testHarness.goto();
+
+        await testHarness.setField('narrowBehaviourNames', narrowBehaviour);
+
+        await testHarness.setField('total', 100);
+
+        await use(testHarness);
+      },
+    });
+
+    testWithBehaviour.describe(`with ${JSON.stringify(narrowBehaviour)}`, () => {
+      for (const width of testWidths) {
+        testWithBehaviour(
+          `renders correctly with viewport width ${width}px`,
+          async ({ page, testHarness }) => {
+            await page.setViewportSize({ width, height: 700 });
+
+            await testHarness.waitForNextFrame();
+
+            const paginationHtml = await testHarness.getPaginationHtml();
+
+            expect(paginationHtml).toMatchSnapshot();
+          },
+        );
+      }
+    });
+  }
+});
