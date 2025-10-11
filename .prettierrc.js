d5096a74a43bb6a803a7768ddbe34c0392e6d091
jonelantha <57597570+jonelantha@users.noreply.github.com>
2025-10-11 10:08:36 +0100

    build: remove duplicate prettier dependency

--- .prettierrc.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('prettier').Config} */
-export default {
-  trailingComma: 'all',
-  tabWidth: 2,
-  semi: true,
-  singleQuote: true,
-  printWidth: 85,
-  arrowParens: 'avoid',
-};
