2aa8b1f62d5f1aef5b35cd9ee733950ec8bdaf79
jonelantha <57597570+jonelantha@users.noreply.github.com>
2025-10-18 09:13:36 +0100

    test: test harnesses assert expected React versions

--- packages/test-harness-react-17/src/main.tsx
+++ packages/test-harness-react-17/src/main.tsx
@@ -1,6 +1,11 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
 import { TestHarnessApp } from 'test-harness-lib';
 
 import 'test-harness-lib/index.css';
+
+if (!React.version.startsWith('17.')) {
+  throw new Error(`Unexpected React version: ${React.version}`);
+}
 
 ReactDOM.render(<TestHarnessApp />, document.getElementById('root'));
--- packages/test-harness-react-18/src/main.tsx
+++ packages/test-harness-react-18/src/main.tsx
@@ -1,7 +1,12 @@
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { TestHarnessApp } from 'test-harness-lib';
 
 import 'test-harness-lib/index.css';
+
+if (!React.version.startsWith('18.')) {
+  throw new Error(`Unexpected React version: ${React.version}`);
+}
 
 const root = createRoot(document.getElementById('root')!);
 
