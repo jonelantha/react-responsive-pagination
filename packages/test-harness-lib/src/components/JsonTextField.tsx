ba4df78ec99874da159ed744a6bfedc3b841ff63
jonelantha <57597570+jonelantha@users.noreply.github.com>
2026-01-29 20:43:37 +0000

    test(test-harness): JsonTextField improved handling of invalid values

--- packages/test-harness-lib/src/components/JsonTextField.tsx
+++ packages/test-harness-lib/src/components/JsonTextField.tsx
@@ -1,16 +1,43 @@
 import { useField } from 'formik';
 import { stringifyWithUndefined, tryJsonParse } from '../test-support/util';
+import { useCallback, useEffect, useState } from 'react';
 
 type JsonTextFieldProps = { name: string } & React.HTMLAttributes<HTMLInputElement>;
 
 export function JsonTextField({ name, ...rest }: JsonTextFieldProps) {
-  const [_, meta, helpers] = useField(name);
+  const [_, { value: parentValue }, { setValue: setParentValue }] = useField(name);
+
+  const [localValue, setLocalValue] = useState(stringifyWithUndefined(parentValue));
+
+  // sync local value when parent value changes
+  useEffect(() => {
+    setLocalValue(stringifyWithUndefined(parentValue));
+  }, [parentValue]);
+
+  const resetToParentValue = useCallback(() => {
+    setLocalValue(stringifyWithUndefined(parentValue));
+  }, [parentValue]);
+
+  const setValue = useCallback(
+    (newValue: string) => {
+      setLocalValue(newValue);
+
+      const parsedValue = tryJsonParse(newValue);
+
+      // does the parsed value convert back to the same string?
+      if (stringifyWithUndefined(parsedValue) === newValue) {
+        setParentValue(parsedValue);
+      }
+    },
+    [setParentValue],
+  );
 
   return (
     <input
       type="text"
-      value={stringifyWithUndefined(meta.value)}
-      onChange={e => helpers.setValue(tryJsonParse(e.target.value))}
+      value={localValue}
+      onChange={e => setValue(e.target.value)}
+      onBlur={resetToParentValue}
       spellCheck={false}
       {...rest}
     />
